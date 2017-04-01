2dca8f67fdfb547d399429dee3e97bb876b1e89d
Shouri Piratla <shouri.piratla9@gmail.com>
2017-04-01 15:53:36 +0530

    Add new urls

--- ppractice/urls.py
+++ ppractice/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
 	url(r'^login/$',views.login,name='login'),
 	url(r'^appointments/(?P<date>)/$',views.appointments,name='appointments'),
 	url(r'^new_appointment/$',views.new_appointment,name='new_appointments'),
+	url(r'^cancel_appointment/$',views.cancel_appointment,name='cancel_appointment'),
+	url(r'^doctor_details/$',views.doctor_details,name='doctor_details')
 ]

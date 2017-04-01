850dc425f59097c12504e8f44eefa09e4b79efae
Shouri Piratla <shouri.piratla9@gmail.com>
2017-04-01 16:05:52 +0530

    Define Models for the application

--- ppractice/models.py
+++ ppractice/models.py
@@ -1,18 +1,29 @@
 from __future__ import unicode_literals
 from django.db import models
 
-class Doctor(model.Models):
+class Doctor(models.Model):
 	GENDER_CHOICES = (
 			('M','Male'),
 			('F','Female')
 		)
 	id = models.AutoField(primary_key = True)
-	name = models.CharField(maxlength = 200)
+	name = models.CharField(max_length = 200)
 	age = models.IntegerField()
-	gender = models.CharField(maxlength = 1,choices = GENDER_CHOICES)
+	gender = models.CharField(max_length = 1,choices = GENDER_CHOICES)
 	clinic_address = models.TextField()
 
-class Appointment(models.Models):
-	
+class Patient(models.Model):
+	GENDER_CHOICES = (
+			('M','Male'),
+			('F','Female')
+		)
+	id = models.AutoField(primary_key = True)
+	name = models.CharField(max_length = 200)
+	age = models.IntegerField()
+	gender = models.CharField(max_length = 1,choices = GENDER_CHOICES)
+	problem_description = models.TextField()
+
+class Appointment(models.Model):
 	date = models.DateTimeField()
 	doctor = models.ForeignKey(Doctor,on_delete = models.CASCADE)
+	patient = models.ForeignKey(Patient,on_delete = models.CASCADE)

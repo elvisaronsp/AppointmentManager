3bda5ba66488f0f902b7ed33d8a942c0088560a4
Shouri Piratla <shouri.piratla9@gmail.com>
2017-04-02 16:33:21 +0530

    Build a dictionary of the db results

--- ppractice/views.py
+++ ppractice/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from ppractice.models import *
 
+@csrf_exempt
 def login(request):
 	pass
 
+@csrf_exempt
 def update_appointments(request):
 	
 	p_id = request.POST.get('pid')
@@ -15,13 +19,13 @@ def update_appointments(request):
 	appt = Appointment.apps.get(doctor=doctor,patient=patient)
 	appt.update(date=date,status=True)
 
+@csrf_exempt
 def appointments(request):
 
 	p_id = request.POST.get('pid')
 	d_id = request.POST.get('did')
 	patient = Patient.pats.get(id=p_id)
 	doctor = Doctor.docs.get(id=d_id)
-	result = []
 	
 	if request.POST.get('requestType') == 'N':	
 		new_app = Appointment(doctor=doctor,patient=patient,status=False)
@@ -32,12 +36,24 @@ def appointments(request):
 		else:
 			result = Appointment.apps.get(patient=patient).values()
 	
+	data = []
+	for res in result:
+		item = {
+			'date' : res['date'],
+			'doctor' : res['doctor'],
+			'patient' : res['patient'],
+			'status' : res['status'],
+			'cancelled_by' : res['cancelled_by']
+		}
+		data.append(item)
+	
 	response['result'] = 'success'
-	respone['data'] = result
+	respone['data'] = data
+
 	return JsonResponse(response)
 
+@csrf_exempt
 def cancel_appointment(request):
-
 	p_id = request.POST.get('pid')
 	d_id = request.POST.get('did')
 	patient = Patient.pats.get(id=p_id)
@@ -50,17 +66,30 @@ def cancel_appointment(request):
 		appt.update(cancelled_by = 'Patient')
 	appt.update(status=False)
 	
-
-def doctor_details(request,id):
+@csrf_exempt
+def doctor_details(request):
 
 	doctors = Doctor.docs.all().values()
+	drs = []
+	for dr in doctors:
+		item = {
+			'name' : dr['name'],
+			'age' : dr['age'],
+			'specialisation' : dr['specialisation'],
+			'gender' : dr['gender'],
+			'clinic_address' : dr['clinic_address']
+		}
+		drs.append(item)
+
+	response = {}
 	response['result'] = 'success'
-	response['data'] = doctors
+	response['data'] = drs
 	return JsonResponse(response)
 
+@csrf_exempt
 def new_user(request):
 	
-	if request.POST.get('userType') == 'D':
+	if request.POST.get('isDoctor'):
 		name = request.POST.get('name')
 		age = int(request.POST.get('age'))
 		gender = request.POST.get('gender')
@@ -75,3 +104,7 @@ def new_user(request):
 		problem_description = request.POST.get('problem_description')
 		pat = Patient(name=name,age=age,gender=gender,problem_description=problem_description)
 		pat.save()
+
+	res = {}
+	res['result'] = 'success'
+	return JsonResponse(res)

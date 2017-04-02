f36c7daaaa2702eac00a9a923ab9dfd7fcba8c1c
Shouri Piratla <shouri.piratla9@gmail.com>
2017-04-02 12:35:45 +0530

    Add new attr. to Doctor and Appointment

--- ppractice/models.py
+++ ppractice/models.py
@@ -2,28 +2,35 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Doctor(models.Model):
+	
 	GENDER_CHOICES = (
 			('M','Male'),
 			('F','Female')
 		)
-	id = models.AutoField(primary_key = True)
+	docs = models.Manager()
 	name = models.CharField(max_length = 200)
 	age = models.IntegerField()
 	gender = models.CharField(max_length = 1,choices = GENDER_CHOICES)
 	clinic_address = models.TextField()
+	specialisation = models.TextField()
 
 class Patient(models.Model):
+	
 	GENDER_CHOICES = (
 			('M','Male'),
 			('F','Female')
 		)
-	id = models.AutoField(primary_key = True)
+	pats = models.Manager()
 	name = models.CharField(max_length = 200)
 	age = models.IntegerField()
 	gender = models.CharField(max_length = 1,choices = GENDER_CHOICES)
 	problem_description = models.TextField()
 
 class Appointment(models.Model):
-	date = models.DateTimeField()
+
+	date = models.DateTimeField(null=True)
 	doctor = models.ForeignKey(Doctor,on_delete = models.CASCADE)
+	apps = models.Manager()
 	patient = models.ForeignKey(Patient,on_delete = models.CASCADE)
+	status = models.BooleanField()
+	cancelled_by = models.CharField(max_length=20)

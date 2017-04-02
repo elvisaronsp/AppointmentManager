35cad153da789fdc4ba9f30eea1ae374f7cfea67
Shouri Piratla <shouri.piratla9@gmail.com>
2017-04-02 12:35:20 +0530

    Update urls

--- ppractice/urls.py
+++ ppractice/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
 	url(r'^login/$',views.login,name='login'),
-	url(r'^appointments/(?P<date>)/$',views.appointments,name='appointments'),
-	url(r'^new_appointment/$',views.new_appointment,name='new_appointments'),
+	url(r'^appointments/$',views.appointments,name='appointments'),
 	url(r'^cancel_appointment/$',views.cancel_appointment,name='cancel_appointment'),
-	url(r'^doctor_details/$',views.doctor_details,name='doctor_details')
+	url(r'^doctor_details/$',views.doctor_details,name='doctor_details'),
+	url(r'^new_user/$',views.new_user,name='new_user')
 ]

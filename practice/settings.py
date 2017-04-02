986a7adc2991df1e32d636d7a23e20b6e7269a0d
Shouri Piratla <shouri.piratla9@gmail.com>
2017-04-02 15:45:55 +0530

    Add app config

--- practice/settings.py
+++ practice/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     
-    'ppractice',
+    'ppractice.apps.PpracticeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

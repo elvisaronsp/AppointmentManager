4100a6e7cf764b022f5986fbc992e65875012236
Shouri Piratla <shouri.piratla9@gmail.com>
2017-04-03 22:41:12 +0530

    Add urls for login and logout

--- ppractice/urls.py
+++ ppractice/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-
-	url(r'^login/$',views.login,name='login'),
+	url(r'^loguserin/$',views.login,name='loguserin'),
 	url(r'^appointments/$',views.appointments,name='appointments'),
 	url(r'^cancel_appointment/$',views.cancel_appointment,name='cancel_appointment'),
 	url(r'^doctor_details/$',views.doctor_details,name='doctor_details'),
 	url(r'^new_user/$',views.new_user,name='new_user'),
-	url(r'^update_appt/$',views.update_appointments,name='update_appts')
+	url(r'^update_appt/$',views.update_appointments,name='update_appts'),
+	url(r'^loguserout/$',views.loguserout,name='loguserout')
 ]

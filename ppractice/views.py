abe1abf2c402efe625c0b56c0d54878b30e03516
Shouri Piratla <shouri.piratla9@gmail.com>
2017-04-03 22:41:32 +0530

    Authenticate User

--- ppractice/views.py
+++ ppractice/views.py
@@ -1,22 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import *
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from ppractice.models import *
 
+from django.contrib.auth import login
+from django.contrib.auth import logout
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+
 @csrf_exempt
-def login(request):
-	pass
+def loguserin(request):
+	
+	email = request['email']
+	password = request['password']
+	user = authenticate(email=email,password=password)
+	if user is not None:
+		login(request,user)
+		redirect('/appointments')
+	else:
+		return HttpResponse("Invalid Credentials. Please try again")
+
+@csrf_exempt
+def loguserout(request):
+	Doctor.docs.all().delete()
+	return HttpResponse("Deleted")
+	# logout(request)
 
 @csrf_exempt
 def update_appointments(request):
-	
+
 	p_id = request.POST.get('pid')
 	d_id = request.POST.get('did')
 	patient = Patient.pats.get(id=p_id)
 	doctor = Doctor.docs.get(id=d_id)
 	date = request.POST.get('date')
 	appt = Appointment.apps.get(doctor=doctor,patient=patient)
+	
 	appt.update(date=date,status=True)
 
 @csrf_exempt
@@ -49,7 +69,6 @@ def appointments(request):
 	
 	response['result'] = 'success'
 	respone['data'] = data
-
 	return JsonResponse(response)
 
 @csrf_exempt
@@ -89,22 +108,23 @@ def doctor_details(request):
 @csrf_exempt
 def new_user(request):
 	
-	if request.POST.get('isDoctor'):
-		name = request.POST.get('name')
-		age = int(request.POST.get('age'))
-		gender = request.POST.get('gender')
+	username = request['email']
+	password = request['password']
+	user = User(username=username,password=password)
+	name = request.POST.get('name')
+	age = int(request.POST.get('age'))
+	gender = request.POST.get('gender')
+	user.save()
+
+	if request.POST.get('isDoctor'):		
 		specialisation = request.POST.get('specialisation')
 		clinic_address = request.POST.get('clinic_address')
-		doc = Doctor(name=name,age=age,gender=gender,specialisation=specialisation,clinic_address=clinic_address)
+		doc = Doctor(user=user,name=name,age=age,gender=gender,specialisation=specialisation,clinic_address=clinic_address)
 		doc.save()
 	else:
-		name = request.POST.get('name')
-		age = int(request.POST.get('age'))
-		gender = request.POST.get('gender')
 		problem_description = request.POST.get('problem_description')
-		pat = Patient(name=name,age=age,gender=gender,problem_description=problem_description)
+		pat = Patient(user=user,name=name,age=age,gender=gender,problem_description=problem_description)
 		pat.save()
-
 	res = {}
 	res['result'] = 'success'
 	return JsonResponse(res)
